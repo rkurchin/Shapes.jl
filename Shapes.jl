d106df080836d02029a435b855aee65917cd002a
Rachel Kurchin <rkurchin@cmu.edu>
2021-10-01 15:00:52 -0400

    add AbstractParallelogram type and area dispatch, dispatch width() and height() on Square and Rectangle

--- Shapes.jl
+++ Shapes.jl
@@ -1,18 +1,23 @@
-
-struct Square
+abstract type AbstractParallelogram end
+area(p::AbstractParallelogram) = width(p) * height(p)
+struct Square <: AbstractParallelogram
     a::Real
 end
+width(s::Square) = s.a
+height(s::Square) = s.a
 
+struct Rectangle <: AbstractParallelogram
+    a::Real
+    b::Real
+end
+width(r::Rectangle) = r.a
+height(r::Rectangle) = r.b
 struct Circle
     r::Real
 end
 
-function area(s::Square)
-    s.a^2
-end
-
 area(c::Circle) = π*c.r^2
 
-import Base.*
+# import Base.*
 
-*(c::Real, s::Square) = Square(c*s.a)
+# Base.*(c::Real, s::Square) = Square(c*s.a)
